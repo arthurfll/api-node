e64c6e6c07070bf6b0aee54741bafef40ea305aa
arthurfll <arthurfontenelle2017@gmail.com>
2023-12-19 11:05:48 -0300

    reiniciado o projeto

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,17 +1,3 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-
-pub mod services;
-
-use services::user::{login,cadastro};
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(login)
-            .service(cadastro)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+fn main() {
+    println!("Hello, world!");
 }
