4f0f90ef990018c6009f3fa6f4a878357bc00ddb
arthurfll <arthurfontenelle2017@gmail.com>
2023-12-22 21:39:23 -0300

    se decide mah

--- rust/target/debug/deps/rust.d
+++
@@ -1,5 +0,0 @@
-D:\legado_api\rust\target\debug\deps\rust.exe: src\main.rs
-
-D:\legado_api\rust\target\debug\deps\rust.d: src\main.rs
-
-src\main.rs:
--- rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-D:\legado_api\rust\target\debug\rust.exe: D:\legado_api\rust\src\main.rs
