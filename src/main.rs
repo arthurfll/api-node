1d3369e78256208c04ecc34943404b7b6d67ad37
arthurfll <arthurfontenelle2017@gmail.com>
2024-02-01 22:43:53 -0300

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use actix_web::{ App , HttpServer };
+use actix_web::{ App , HttpServer , web };
 use std::env;
 use dotenv::dotenv;
+use sqlx::MySqlPool;
+use std::sync::Arc;
 
 use services::{
     usuario::{
@@ -10,7 +12,7 @@ use services::{
         mensagens,enviar
     },
     conversa::{
-        conversas,delete
+        conversas,delete,criar
     }
 };
 
@@ -18,20 +20,24 @@ use services::{
 
 #[actix_web::main]
 async fn main()-> std::io::Result<()> {
-
     dotenv().ok();
-
     let port = env::var("PORT")
         .unwrap_or_else(|_| String::from("8080"));
 
-    HttpServer::new(|| {
+    let db_url = "mysql://root:@localhost:3306/arthurfll";
+    let pool = MySqlPool::connect(db_url).await.unwrap();
+    println!("conectado ao banco de dados");
+
+    HttpServer::new(move|| {
         App::new()
+            .app_data(web::Data::new(Arc::new(pool.clone())))
             .service(login)
             .service(logout)
             .service(cadastro)
             .service(mensagens)
             .service(enviar)
             .service(conversas)
+            .service(criar)
             .service(delete)
     })
     .bind(format!("127.0.0.1:{}", port))?
--- src/services/usuario.rs
+++ src/services/usuario.rs
@@ -1,10 +1,34 @@
-use actix_web::{get,Responder,HttpResponse};
+use actix_web::{HttpResponse,Responder,get,post,web};
+use crate::models::usuario::Cadastro;
+use sqlx;
+use std::sync::Arc;
+use sqlx::MySqlPool;
+use actix_web::web::Data;
 
 
-#[get("/cadastro")]
-pub async fn cadastro() -> impl Responder {
-    HttpResponse::Ok().json("cadastro")
+
+#[post("cadastro/")]
+pub async fn cadastro(cadastro: web::Json<Cadastro>, pool: Data<Arc<MySqlPool>>) -> HttpResponse {
+    let query = format!(
+        r#"
+            INSERT INTO usuario (username, first_name, last_name, email, password, saldo)
+            VALUES ('{}', '{}', '{}', '{}', '{}', {})
+        "#,
+        cadastro.username,
+        cadastro.first_name,
+        cadastro.last_name,
+        cadastro.email,
+        cadastro.password,
+        cadastro.saldo
+    );
+
+    let _ = sqlx::query(&query)
+        .execute(&***pool)
+        .await;
+    println!("200");
+    return HttpResponse::Ok().json(cadastro)
 }
+
 
 #[get("/login")]
 pub async fn login() -> impl Responder {
