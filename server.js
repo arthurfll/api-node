6d397d8f955ba68f198cb41238c7d789017491e8
arthurfll <arthurfontenelle2017@gmail.com>
2024-04-26 19:05:07 -0300

    middleware logger

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const path = require("path")
 const PORT = process.env.PORT || 8888
 const { logger } = require("./middleware/logger")
+const errorHandler = require("./middleware/errorHandler")
+
 
 app.use(logger)
 app.use(express.json())
@@ -21,6 +23,8 @@ app.all("*",(req,res) => {
         res.type("txt").send("404 Not Found")
     }
 })
+
+app.use(errorHandler)
 
 app.listen(PORT, ()=> console.log("server running on port 8888"))
 
