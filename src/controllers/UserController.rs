3e0b2fce4d4de1ce1156e5e3e013767484fab810
arthurfll <arthurfontenelle2017@gmail.com>
2024-04-26 17:56:33 -0300

    middleware logger

--- src/controllers/UserController.rs
+++
@@ -1,6 +0,0 @@
-use actix_web::{get,HttpResponse,Responder};
-
-#[get("/all_users/")]
-pub async fn all_users() -> impl Responder {
-    HttpResponse::Ok().body("selva")
-}
--- src/routes/UserRoute.rs
+++
@@ -1,9 +0,0 @@
-use actix_web::web;
-use crate::controllers::UserController::{all_users};
-
-pub fn UserRoute(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::scope("/user/")
-            .service(all_users)
-    );
-}
